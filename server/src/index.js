867fc3c10f7d8c4d3f9849a108f227af396e165e
asifmujtoba <46132982+asifmujtoba@users.noreply.github.com>
2021-01-22 05:35:51 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 dotenv.config();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 // stablish connection to DB
 const { connectToDB } = require('./database');
